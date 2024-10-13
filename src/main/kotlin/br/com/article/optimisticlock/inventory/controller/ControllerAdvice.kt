6bedb3e7aa2d34cf2efa214569bf88c68684858e
William Cesar Santos <william_cesar_santos@hotmail.com>
2024-10-13 17:00:50 -0300

    Fix imports

--- src/main/kotlin/br/com/article/optimisticlock/inventory/controller/ControllerAdvice.kt
+++ src/main/kotlin/br/com/article/optimisticlock/inventory/controller/ControllerAdvice.kt
@@ -5,8 +5,8 @@ import br.com.article.optimisticlock.inventory.service.InventoryNotFoundExceptio
 import org.hibernate.StaleObjectStateException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ControllerAdvice
 
 @ControllerAdvice
 class ControllerAdvice {
--- src/test/kotlin/br/com/article/optimisticlock/inventory/controller/InventoryControllerIntegrationTest.kt
+++ src/test/kotlin/br/com/article/optimisticlock/inventory/controller/InventoryControllerIntegrationTest.kt
@@ -1,7 +1,11 @@
 package br.com.article.optimisticlock.inventory.controller
 
+import br.com.article.optimisticlock.inventory.model.Inventory
+import br.com.article.optimisticlock.inventory.service.InventoryConflictException
+import br.com.article.optimisticlock.inventory.service.InventoryNotFoundException
 import br.com.article.optimisticlock.inventory.service.InventoryService
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -24,6 +28,9 @@ class InventoryControllerIntegrationTest {
 
     @Test
     fun `decrementInventory success`() {
+        `when`(service.decrementInventory(1))
+            .thenReturn(Inventory(1, 9, 1))
+
         mockMvc.perform(put("/inventory/1/decrement"))
             .andExpect(status().isOk)
             .andExpect(jsonPath("$.quantity").value(9))
@@ -31,12 +38,18 @@ class InventoryControllerIntegrationTest {
 
     @Test
     fun `decrementInventory not found`() {
+        `when`(service.decrementInventory(999))
+            .thenThrow(InventoryNotFoundException::class.java)
+
         mockMvc.perform(put("/inventory/999/decrement"))
             .andExpect(status().isNotFound)
     }
 
     @Test
     fun `decrementInventory conflict`() {
+        `when`(service.decrementInventory(1))
+            .thenThrow(InventoryConflictException::class.java)
+
         mockMvc.perform(put("/inventory/1/decrement"))
             .andExpect(status().isConflict)
     }
